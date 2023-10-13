057bc4e72448d7e8712c25d2c72aed9b7f58fae3
Joao Victor <vicjopers@gmail.com>
2023-10-13 09:43:25 -0300

    Tratamento de excecoes finalizado

---
+++ src/aplication/Program.java
@@ -0,0 +1,44 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import exceptions.BusinessException;
+import model.entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe os dados da conta");
+		System.out.print("Numero: ");
+		int number = sc.nextInt();
+		System.out.println("Nome: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Valor inicial: ");
+		double balance = sc.nextDouble();
+		System.out.print("Limite de saque: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		
+		System.out.print("Informe a quatia que deseja sacar: ");
+		double amount = sc.nextDouble();
+		try {
+		acc.withdarw(amount);
+		System.out.printf("Novo saldo: %.2f%n", acc.getBalance());
+		}
+		catch(BusinessException e){
+			System.out.println(e.getMessage());
+		}
+
+		
+		sc.close();
+	}
+
+}
---
+++ src/model/entities/Account.java
@@ -0,0 +1,97 @@
+package model.entities;
+
+import exceptions.BusinessException;
+
+public class Account {
+	
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+	
+	public Account() {
+		
+	}
+
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	public void setWithdrawLimit(Double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public void deposit(double amount) {
+		balance -= amount;
+	}
+	
+	public void withdarw(double amount) {
+		ValidadeWithdraw(amount);
+		balance -= amount;
+	}
+
+	public String toString() {
+		return "O numero da conta: "+number;
+	}
+	
+	public void ValidadeWithdraw(double amount) {
+		if(amount > getWithdrawLimit()) {
+			throw new BusinessException ("Erro de saque: a quantidade excede o limite de saque");
+		}
+		if(amount>getBalance()) {
+			throw new BusinessException("Erro de saque: Saldo insuficiente");
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
